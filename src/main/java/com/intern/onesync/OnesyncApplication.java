c1b984d7ac096d64386937058da9d0abaa4f55ea
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-12-12 22:07:11 +0900

    init

---
+++ src/main/java/com/intern/onesync/OnesyncApplication.java
@@ -0,0 +1,13 @@
+package com.intern.onesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnesyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intern/onesync/OnesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.onesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
